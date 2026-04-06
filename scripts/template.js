ce54edfa3fc7d628211d34822b4b46294ac2f223
Jonas Hildebrand <jhildebrand94@googlemail.com>
2026-04-07 01:52:15 +0200

    type-badge changed in templates/ changes in css for pokemon cards

--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,3 @@
-function getPokemonTemplate(pokemon, index) {
-
  const typeColors = {
     fire: '#ff9d55', water: '#5090d6', grass: '#63bc5a', bug: '#9bcc50',
     normal: '#a8a878', electric: '#eed535', poison: '#b97fc9', flying: '#3dc7ef',
@@ -8,20 +6,25 @@ function getPokemonTemplate(pokemon, index) {
     dragon: '#7038f8', dark: '#705848'
   };
 
+
+function getPokemonTemplate(pokemon, index) {
+
  const type1 = pokemon.types[0].type.name;
  const type2 = pokemon.types[1] ? pokemon.types[1].type.name : type1;
-
  const color1 = typeColors[type1];
  const color2 = typeColors[type2];
 
   return /*html*/ `
       <div class="pokemon_card" onclick="openPokemonCardDialog(${index})">
-       <img src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}" class="pokemon_img" style="background: linear-gradient(135deg, ${color1} 50%, ${color2} 50%)";> 
+       <img src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}" class="pokemon_img" style="background: linear-gradient(135deg, ${color1} 50%, ${color2} 50%)"> 
         <div class="number_name_types">
          <p class="pokedex_number">Nr. ${String(index + 1).padStart(4, '0')}</p>
          <h2>${pokemon.name.toUpperCase()}</h2>
           <div class="types-container">
-           ${pokemon.types.map(t => `<span class="type-${t.type.name}">${t.type.name}</span>`).join(' ')}
+           ${pokemon.types.map(t => {
+             const bgColor = typeColors[t.type.name];
+             return `<span class="type-badge" style="background-color: ${bgColor}">${t.type.name}</span>`;
+           }).join(' ')}
           </div>
         </div>
       </div>
@@ -31,6 +34,7 @@ function getPokemonTemplate(pokemon, index) {
 // String(index + 1).padStart(4, '0') / .padStart is a string method -> mandatory to change number to string
 // From number to "number" / (4, '0') -> "Text" should have for "Letters", 0 to fill the space if "number" is shorter
 // linear-gradient(135deg, ${color1} 50%, ${color2} 50%)"; -> show both types if Pokémon has two types in the background
+
 
 function getPokemonCardTemplate(pokemon, index) {
 
@@ -42,7 +46,10 @@ function getPokemonCardTemplate(pokemon, index) {
            <p>height: ${(pokemon.height / 10).toFixed(1).replace(".", ",")} m</p>
            <p>weight: ${(pokemon.weight / 10).toFixed(1).replace(".", ",")} kg</p>
             <div class="types-container">
-             ${pokemon.types.map(t => `<span class="type-${t.type.name}">${t.type.name}</span>`).join(' ')}
+             ${pokemon.types.map(t => {
+              const bgColor = typeColors[t.type.name];
+              return `<span class="type-badge" style="background-color: ${bgColor}">${t.type.name}</span>`;
+             }).join(' ')}
             </div>
       </div>
       `;
